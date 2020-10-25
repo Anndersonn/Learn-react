77ea71b21cba658459c2d837a9521658d6d85c5a
Anndersonn <slobodjanik1997@gmail.com>
2020-10-25 14:33:16 +0100

    added functionality to navbar, made redux folder

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 import {BrowserRouter, Route} from "react-router-dom";
 
+
 const App = (props) => {
+
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div class='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/> 
-                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' render={ () => <Dialogs dialogs={props.state.dialogs} messages={props.state.messages}/>}/> 
+                    <Route path='/profile' render={ () => <Profile posts={props.state.posts}/>}/>
+                    <Route path ='/news' render={ () => <News />} />
+                    <Route path ='/music' render={ () => <Music />} />
+                    <Route path ='/settings' render={ () => <Settings />} />
                 </div>
             </div>
         </BrowserRouter>)
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
-import DialogItem from './DialogItem/Dialogs';
+import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
 
@@ -9,25 +8,10 @@ import Message from './Message/Message';
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrew'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Viktor'},
-        {id: 6, name: 'Valera'}
-    ]
+    debugger;
 
-    let messages = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How is your it-kamasutra?'},
-        {id: 3, message: 'Yo'},
-        {id: 4, message: 'Yo'},
-        {id: 5, message: 'Yo'}
-    ]
-
-    let dialogsElements =  dialogs.map( d => <DialogItem name={d.name} id={d.id} />  );
-    let messagesElements = messages.map( m => <Message message={m.message}/> );
+    let dialogsElements =  props.dialogs.map( d => <DialogItem name={d.name} id={d.id} />  );
+    let messagesElements = props.messages.map( m => <Message message={m.message}/> );
 
     return (
         <div className={s.dialogs}>
