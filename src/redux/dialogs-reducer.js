845fb6f9dfc2c8e56b734f457ef02bb44f184cbc
Anndersonn <slobodjanik1997@gmail.com>
2020-11-09 11:30:22 +0100

    migration from state to redux

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -2,7 +2,27 @@ const ADD_BODY = 'ADD-BODY';
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 
 
-const dialogsReducer = (state, action) => {
+let initialDialogs = { 
+    dialogs: [
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Andrew' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Viktor' },
+        { id: 6, name: 'Valera' },
+    ],
+
+    messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How is your it-kamasutra?' },
+        { id: 3, message: 'Yo' },
+        { id: 4, message: 'Yo' },
+        { id: 5, message: 'Yo' }
+    ],
+    newMessageBody: ''
+}
+
+const dialogsReducer = (state = initialDialogs, action) => {
     switch (action.type) { 
         case ADD_BODY:
             let newBody = {
